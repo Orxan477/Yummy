248c12ed9fba9be56456170c546b08f25e0edec9
Orxan477 <tu201906147@code.edu.az>
2023-10-26 14:52:35 +0400

    layouts-fixed

--- resources/views/back/index.blade.php
+++ resources/views/back/index.blade.php
@@ -1,1 +1,2 @@
-test
+@extends('back.layouts.master')
+
---
+++ resources/views/back/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('front.layouts.head')    
+    @yield('css')
+</head>
+<body>
+    <!--  Body Wrapper -->
+  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+    data-sidebar-position="fixed" data-header-position="fixed">
+        @include('back.layouts.sidebar')
+    <!--  Sidebar End -->
+        <!--  Main wrapper -->
+            <div class="body-wrapper">
+        <!--  Header Start -->
+        @include('back.layouts.header')
+         <!--  Header End -->
+            @yield('content')  
+            @include('front.layouts.footer')
+            @include('front.layouts.script')
+@           yield('js')
+</body>
+</html>
