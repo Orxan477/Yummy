ce65ed351acaa1a22a69d0f2f6517190cd6907f6
Orxan477 <tu201906147@code.edu.az>
2023-10-30 15:21:22 +0400

    feedback create

---
+++ database/migrations/2023_10_30_110828_create_table_feedback.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullName',50);
+            $table->text('content');
+            $table->string('image',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feedbacks');
+    }
+};
